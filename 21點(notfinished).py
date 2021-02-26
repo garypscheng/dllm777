ce1b94b1ae889ad4272d27150da90aa0a8d62c6f
ppkkkpk <79691025+ppkkkpk@users.noreply.github.com>
2021-02-27 02:04:45 +0800

    Update 21點(notfinished).py

--- 21點(notfinished).py
+++ 21點(notfinished).py
@@ -1,6 +1,5 @@
 import random  # 隨機效果
 import time  # 計時效果
-import operator
 
 suit = ["Spade", "Heart", "Club", "Diamond"]
 face = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
@@ -51,7 +50,16 @@ def scoring():
         sum = Playerpoint1.get(player) + ptt
         Playerpoint1[player] = sum
         time.sleep(1)
-        print(player, Num, " total point:", sum)
+        if a == 5:
+            delay()
+            print(player, Num, " total point:", sum)
+        elif zzz == 100:
+            if player == Player1:
+                print(player, "decides to Hit, and receives", draw, "total:", sum)
+            elif player != Player1:
+                delay()
+                print(player, "decides to Hit, and receives", draw)
+
 
     def calunknown():  # 暗牌部分
         for player in Playerpoint1:
@@ -76,39 +84,15 @@ def scoring():
             time.sleep(1)
             print(player, "Unknown", "total point: Unknown")
 
-    def decisioncal():  # hit/stand 部分
-        draw = random.choice(list(point))
-        time.sleep(1)
-        print(player, "has draw", draw)
-        Cpt = point.get(draw)
-        Num = Playerpoint2.get(player) + " " + draw
-        Playerpoint2[player] = Num
-        if Cpt == "(1, 11)":
-            if Playerpoint1[player] > 10:
-                pt = 1
-            else:
-                pt = 11
-        else:
-            pt = int(Cpt)
-        ptt = pt
-        sum = Playerpoint1.get(player) + ptt
-        Playerpoint1[player] = sum
-
     def exclusion():
         time.sleep(1)
-        print(player, "has Stand")
+        print(player, "decides to Stand")
         delay()
-        minus.remove(player)
+        # minus.remove(player)  #打算用黎remove stand 既玩家避免重覆printing ,
+        # 但出 ValueError: list.remove(x): x not in list  --> 待解決
 
-    def hit1():
+    def hit():
         time.sleep(1)
-        print(player, "has hit")
-        delay()
-        decisioncal()
-
-    def hit2():
-        time.sleep(1)
-        print(player, "Hit")
         delay()
         commoncal()
 
@@ -119,50 +103,44 @@ def scoring():
 
     Playerpoint1 = {Player1: 0, "player 2": 0, "player 3": 0, "player 4": 0, "Banker": 0}  # 計點數
     Playerpoint2 = {Player1: "", "player 2": "", "player 3": "", "player 4": "", "Banker": ""}  # 記牌
-    minus = [Player1, "player 2", "player 3", "player 4", "Banker"]
+    #minus = [Player1, "player 2", "player 3", "player 4", "Banker"]
 
     print("\n\nNew Round\nWith Card Exhibiting")
     for player in Playerpoint1:
+        a = 5
         commoncal()
+        a = 0
 
     print("\n\nWith Card Covered")
     calunknown()
 
     print("\n\n")
-    ask = input("It's your turn now, stand or hit?").lower()  # 分歧: 玩家決定stand or hit
-    if ask == "stand":  # 獨立事件1: 玩家決定stand
-        for player in Playerpoint1:
-            if player == Player1:  # 玩家 stand
-                exclusion()
-            elif player != Player1 and Playerpoint1[player] > 15:  # 大過15點電腦: stand
-                exclusion()
-            elif player != Player1 and Playerpoint1[player] < 15:  # 細過15點電腦: hit
-                hit1()
-                if player != Player1 and Playerpoint1[player] < 15:
-                    hit1()
-    elif ask == "hit":  # 獨立事件2: 玩家決定hit
-        for player in Playerpoint1:
-            if player == Player1:
-                hit2()
-                ask = input("It's your turn now, stand or hit?").lower()
-                if ask == "stand":
-                    exclusion()
-                elif ask == "hit":
-                    hit2()
-                    ask = input("It's your turn now, stand or hit?").lower
-                    if ask == "stand":
-                        print(player, "has Stand")
-                    elif ask == "hit":
-                        hit2()
-            elif player != Player1 and Playerpoint1[player] < 15:
-                hit1()
-                if player != Player1 and Playerpoint1[player] < 15:
-                    hit1()
+    HitAndSt = True
+    while HitAndSt == True:
+        ask = input("stand or hit").lower()
+        if ask == "hit":  # 獨立事件2: 玩家決定hit
+            zzz = 100
+            for player in Playerpoint1:
+                if player == Player1:
+                    hit()
+                elif player != Player1 and Playerpoint1[player] < 15:
+                    hit()
                 elif player != Player1 and Playerpoint1[player] > 15:
                     exclusion()
-            elif player != Player1 and Playerpoint1[player] > 15:
-                exclusion()
 
+        elif ask == "stand":
+            zzz = 100
+            HitAndSt = False
+            for player in Playerpoint1:
+                if player == Player1 or player != Player1 and Playerpoint1[player] > 15:
+                    exclusion()
+                    continue# 細過15點的電腦玩家: hit
+                hit()
+                break
+        elif ask != "hit" and ask !="stand":
+            print("Sorry, I don't understand.")
+    target = 21
+    zzz = 0
     for player in Playerpoint1:
         if Playerpoint1[player] > 21:
             print(player, "Bust")
