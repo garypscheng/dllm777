816a708185483ca87bef525517d614d393fdfba3
ppkkkpk <79691025+ppkkkpk@users.noreply.github.com>
2021-02-26 19:14:16 +0800

    Update 21點(notfinished).py

--- 21點(notfinished).py
+++ 21點(notfinished).py
@@ -1,31 +1,27 @@
-# 導入模組
 import random                                                                                # 隨機效果
 import time                                                                                  # 計時效果
 import operator
-
-#前設部分
 c = ["Spade", "Heart", "Club", "Diamond"]                                                    # 花色
 v = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]           # 牌面
-n = ["(1, 11)", "2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10"]*4            # 面值 (確保v, n 既list items 係一樣(之後用黎直接整合))
+n = ["(1, 11)", "2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10"]*4            # 面值
 card = []                                                                                    # 即將整既牌set
 point = dict()
 sum = 0                                                                                      # 計total點數
 name = input("Hello, What's your name?")
 time.sleep(1)
 
-# Function Definitions 部分:                                             
-def CardSet():                                                                               # 生成一副牌                          
+def CardSet():
     for w in c:
         for p in v:
             card.append(w+ " "+p)
 
 def PointValue():
-    keys = range(52)                                                                         # 用range integer化 -> 用作loop用途
+    keys = range(52)                                                                          # 用range integer化 -> 用作loop用途
     values = card
     for i in keys:
         point[values[i]] = n[i]
 
-def game():                                                                                  # 開場白
+def game():                                                                                   #    Game Intro
     print("Alright,", name, ", you are going to fight with other players and the banker. Bless you!")
     print("The game will start after 3 seconds")
     time.sleep(3)
@@ -34,24 +30,24 @@ def game():                                                                     
     scoring()
 
 def scoring():
-    def commoncal():                                                                          # 明牌部分calculation
-        draw = random.choice(list(point))                                                     # 隨機抽牌
-        Cpt = point.get(draw)                                                                 # 利用dict功能show key所代表的數值
-        Num = Playerpoint2.get(player) + " " + draw                                           # 記起抽到咩牌
+    def commoncal():                                                                          #    明牌部分
+        draw = random.choice(list(point))
+        Cpt = point.get(draw)
+        Num = Playerpoint2.get(player) + " " + draw
         Playerpoint2[player] = Num
-        if Cpt == "(1, 11)":                                                                  # 計分部分
-            if Playerpoint1[player] > 10:                                                     # Ace 點數根據玩家總得分而決定為1/11
+        if Cpt == "(1, 11)":
+            if Playerpoint1[player] > 10:
                 pt = 1
             else:
                 pt = 11
         else:
-            pt = int(Cpt)                                                                     # 其他牌直接轉換其點數
+            pt = int(Cpt)
         ptt = pt
         sum = Playerpoint1.get(player) + ptt
         Playerpoint1[player] = sum
         print(player, Num, " total point:", sum)
 
-    def calunknown():                                                                        #    暗牌部分calculation
+    def calunknown():                                                                        #    暗牌部分
             for player in Playerpoint1:
                 draw = random.choice(list(point))
                 Cpt = point.get(draw)
@@ -67,12 +63,13 @@ def scoring():
                 ptt = pt
                 sum = Playerpoint1.get(player) + ptt
                 Playerpoint1[player] = sum
-                if player == name:                                                            #    增加了除自己玩家外其他總分為未知數 (暗牌)
+                if player == name:
                     print(player, Num, " total point:", sum)
                     continue
                 print(player, "Unknown", "total point: Unknown")
+            print(Playerpoint1)
 
-    def decisioncal():                                                                  # 用於hit/stand 部分
+    def decisioncal():                                                                  # hit/stand 部分
         draw = random.choice(list(point))
         print(player, "has draw", draw)
         Cpt = point.get(draw)
@@ -87,24 +84,26 @@ def scoring():
             pt = int(Cpt)
         ptt = pt
         sum = Playerpoint1.get(player) + ptt
-        Playerpoint1[player] = sum                                                       # 與commoncal()相比,佢缺少print總分既部分
+        Playerpoint1[player] = sum
 
-    def exclusion():                                                                     # 當玩家決定stand時, 為避免佢地會一直重複講xxx Stand, 而決定將其由list剔出
+    def exclusion():
         print(player, "has Stand")
         minus.remove(player)
 
-    def hit1():                                                                          # hit 計算方式
+    def hit1():
         print(player, "has hit")
         decisioncal()
 
     def hit2():
-        print(player, "has hit")
+        print(player, "Hit")
         commoncal()
-   
+
+
+    a = 5
     Playerpoint1 = {name: 0, "player 1": 0, "player 2": 0, "player 3": 0, "Banker": 0}       #   for 計分用
     Playerpoint2 = {name: "", "player 1": "", "player 2": "", "player 3": "", "Banker": ""}  #   for 計牌用
-    minus = [name, "player 1", "player 2", "player 3", "Banker"]                             #   for 剔除用: 費時不停重覆player 1 has Stand, player 1 has Stand, player 1 has Stand
-        
+    minus = [name, "player 1", "player 2", "player 3", "Banker"]
+
     print("\n\nNew Round\nWith Card Exhibiting")
     for player in Playerpoint1:                                                              #    明牌
         commoncal()
@@ -113,9 +112,23 @@ def scoring():
     calunknown()                                                                             #    暗牌
 
     print("\n\n")
+# 1: 玩家決定stand or hit
+     #a) stand: 玩家不抽, 而cpu判定stand or hit  -->single loop
+     #b) hit: 玩家抽和點數<15的玩家一起抽 -->再問(stand or hit) --> hit: 玩家抽和點數<15的玩家一起抽 --->loop: 直到玩家答stand (while)
+
+# 2: CPU決定stand or hit    --> 用while 或者 if xx= True
+     # ai) 當cpu點數>15:決定stand: 其餘<15的決定抽
+     # aii) 當cpu點數<15: 決定hit --> looping aii) until false -->back to ai)
+# 3: 判定階段:
+     # 全部玩家都stand --> if statement
+     # i) if 點數=21 --> direct winner
+     # ii) if 點數>21 --> 該名玩家直接輸, 其餘的最接近而小於21點的為勝
+     # iii) if 點數<21 :最接近而小於21點的玩家為勝
+
+    
     ask = input("stand or hit?")                                 # 分歧: 玩家決定stand or hit
     askk = ask.lower()
-    if askk == "stand":                                          # 獨立事件1: 玩家決定stand
+    if askk == "stand":                                    # 獨立事件1: 玩家決定stand
         for player in Playerpoint1:
             if player == name:                                               # 玩家 stand
                 exclusion()
@@ -125,17 +138,17 @@ def scoring():
                 hit1()
                 if player != name and Playerpoint1[player] < 15:  # 細過15點的電腦玩家: hit
                     hit1()
-    elif askk == "hit":                                           # 獨立事件2: 玩家決定hit
+    elif askk == "hit":                                    # 獨立事件2: 玩家決定hit
         for player in Playerpoint1:
             if player == name:
                 hit2()
-                ask = input("stand or hit?")                      # 分歧: 玩家決定stand or hit
+                ask = input("stand or hit?")  # 分歧: 玩家決定stand or hit
                 askk = ask.lower()
                 if askk == "stand":
                     exclusion()
                 elif askk == "hit":
                     hit2()
-                    ask = input("stand or hit?")                  # 分歧: 玩家決定stand or hit
+                    ask = input("stand or hit?")  # 分歧: 玩家決定stand or hit
                     askk = ask.lower()
                     if askk == "stand":
                         print(player, "has Stand")
@@ -158,11 +171,12 @@ def scoring():
             break
         elif Playerpoint1[player] == 21:              # 呢度我想整 if 無人=21, then winner = 最接近又細過21既數
             pass
-    print(Playerpoint1)
-    print(Playerpoint2)
+    
+    for player in Playerpoint1:
+        print(player, "has:", Playerpoint2[player], "             with total point:", Playerpoint1[player])
 
 
-# Call Functions 
+
 CardSet()
 PointValue()
 game()
